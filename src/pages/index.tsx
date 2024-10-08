262996e303d3b45795c58be1edd86c19ecbcc4e4
kobe13 <kobe13010@gmail.com>
2024-10-09 01:28:03 +0200

    add eslint config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import type { HeadFC, PageProps } from "gatsby"
 
 const IndexPage: React.FC<PageProps> = () => {
   return (
-      <main className="flex justify-center flex-col text-center p-20" >
+      <main className="flex justify-center flex-col text-center p-20">
         <h1 className="text-3xl font-bold">
           My portfolio 😎
         </h1>
