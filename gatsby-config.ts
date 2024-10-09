ec32cdb21f92cce49516b783e49d35a01bd830ea
kobe13 <kobe13010@gmail.com>
2024-10-10 00:16:58 +0200

    change gatsby config

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -3,7 +3,8 @@ import type { GatsbyConfig } from "gatsby";
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `My portfolio`,
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://kobe13.github.io/portfolio`,
+    pathPrefix: `/portfolio`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
