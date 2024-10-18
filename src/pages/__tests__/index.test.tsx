6983c2d9c73dbf923e9dacd36978df4c642c3aa6
kobe13 <kobe13010@gmail.com>
2024-10-19 01:12:07 +0200

    update tests

--- src/pages/__tests__/index.test.tsx
+++ src/pages/__tests__/index.test.tsx
@@ -17,20 +17,30 @@ const mockPageProps: PageProps<object> = {
   params: {},
 } as PageProps<object>;
 
-describe("IndexPage", () => {
+describe("portfolio page", () => {
   it("renders the main heading with the correct name", () => {
     render(<IndexPage {...mockPageProps} />);
+
     const heading = screen.getByRole("heading", { level: 1 });
+
     expect(heading).toHaveTextContent(`Hi, I'm ${DATA.name}`);
+  });
+
+  it("renders the image component", () => {
+    render(<IndexPage {...mockPageProps} />);
+
+    expect(screen.getByTestId("mock-static-image")).toBeInTheDocument();
   });
 
   it("displays the presentation text", () => {
     render(<IndexPage {...mockPageProps} />);
+
     expect(screen.getByText(DATA.presentation)).toBeInTheDocument();
   });
 
   it("renders social media links", () => {
     render(<IndexPage {...mockPageProps} />);
+
     expect(screen.getByRole("link", { name: /linkedin/i })).toHaveAttribute(
       "href",
       DATA.contact.social.LinkedIn.url,
@@ -47,13 +57,54 @@ describe("IndexPage", () => {
 
   it("renders the skills section", () => {
     render(<IndexPage {...mockPageProps} />);
+
     expect(
       screen.getByRole("heading", { name: /skills/i }),
     ).toBeInTheDocument();
+
+    DATA.skills.forEach((skill) => {
+      expect(screen.getAllByText(skill).length).toBeGreaterThan(0);
+    });
+
+    DATA.keySkills.forEach((skill) => {
+      expect(screen.getByText(skill)).toBeInTheDocument();
+    });
   });
 
-  it("renders the StaticImage component", () => {
+  it("renders the work section", () => {
     render(<IndexPage {...mockPageProps} />);
-    expect(screen.getByTestId("mock-static-image")).toBeInTheDocument();
+
+    expect(
+      screen.getByRole("heading", { name: /latest work experience/i }),
+    ).toBeInTheDocument();
+
+    DATA.work.forEach((job) => {
+      const jobElement = screen.getByText(job.title).closest("section");
+
+      expect(jobElement).toBeInTheDocument();
+      expect(jobElement).toHaveTextContent(job.dates);
+      expect(jobElement).toHaveTextContent(job.location);
+      job.description.forEach((description) => {
+        expect(jobElement).toHaveTextContent(description);
+      });
+      job.skills.forEach((skill) => {
+        expect(jobElement).toHaveTextContent(skill);
+      });
+    });
+  });
+
+  it("renders the get in touch section with correct email", () => {
+    render(<IndexPage {...mockPageProps} />);
+
+    expect(
+      screen.getByRole("heading", { name: /get in touch/i }),
+    ).toBeInTheDocument();
+
+    const emailLink = screen.getByRole("link", { name: /email/i });
+
+    expect(emailLink).toBeInTheDocument();
+    expect(emailLink.getAttribute("href")).toMatch(
+      new RegExp(`^mailto:${DATA.contact.email}`),
+    );
   });
 });
