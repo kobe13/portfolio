d5bd9066740a67ad5aa5407c229dd1a4d426510b
kobe13 <kobe13010@gmail.com>
2024-10-20 14:44:12 +0200

    add ts types for testing-library

--- src/pages/__tests__/index.test.tsx
+++ src/pages/__tests__/index.test.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
-import "@testing-library/jest-dom";
 import { DATA } from "../../data/resume";
 import IndexPage from "../index";
 import { PageProps } from "gatsby";
